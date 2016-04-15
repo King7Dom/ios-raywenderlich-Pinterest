6f61e12b65616df78180913873eee45db7d72fd0
Dominic Cheung <dom.with.a.inic@gmail.com>
2016-04-15 17:59:37 +1000

    Hello World

---
+++ Pinterest/PinterestLayout.swift
@@ -0,0 +1,13 @@
+//
+//  PinterestLayout.swift
+//  Pinterest
+//
+//  Created by Dominic's Macbook Pro on 15/04/2016.
+//  Copyright © 2016 Razeware LLC. All rights reserved.
+//
+
+import UIKit
+
+class PinterestLayout: UICollectionViewLayout {
+
+}
